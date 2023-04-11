4de06126b705bc1fe12edaf80e3babf7045f8ed5
Jaihanuman64 <mohak64bansal@gmail.com>
2023-04-12 00:48:42 +0530

    creating file struc and routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,28 @@
-import { useMemo } from "react"
-import {createTheme} from "@mui/material/styles";
-import { themeSettings } from "./theme"
-import {CssBaseline,ThemeProvider } from "@mui/material";
+import { useMemo } from "react";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import { BrowserRouter,Routes, Route } from "react-router-dom";
+import Navbar from "@/scenes/navbar";
 
 function App() {
-
-  const theme = useMemo(()=> createTheme(themeSettings), [])
+  const theme = useMemo(() => createTheme(themeSettings), []);
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-      <CssBaseline/>
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar/>
+            <Routes>
+              <Route path="/" element={<div>Dashboard page</div>} />
+              <Route path="/prediction" element={<div>Prediction page</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
