6abb3ed4ec0eec51b1a3872bd189a991551924fd
Jaihanuman64 <mohak64bansal@gmail.com>
2023-04-11 21:51:48 +0530

    setting up MUI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
-
+import { useMemo } from "react"
+import {createTheme} from "@mui/material/styles";
+import { themeSettings } from "./theme"
+import {CssBaseline,ThemeProvider } from "@mui/material";
 
 function App() {
 
+  const theme = useMemo(()=> createTheme(themeSettings), [])
   return (
     <div className="app">
-      
+      <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      </ThemeProvider>
     </div>
   )
 }
