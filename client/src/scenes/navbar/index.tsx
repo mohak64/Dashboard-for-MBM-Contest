1ebc0bcb25bfa05f12a7524f12c017f1127a5549
Jaihanuman64 <mohak64bansal@gmail.com>
2023-04-12 01:19:59 +0530

    updating the navbar links with MUI

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import CodeIcon from "@mui/icons-material/Code";
 import { Box, Typography, useTheme } from "@mui/material";
 import FlexBetween from "@/components/FlexBetween";
 
@@ -9,14 +10,42 @@ const Navbar = (props: Props) => {
   const { palette } = useTheme();
   const [selected, setSelected] = useState("dashboard");
   return (
-    <FlexBetween
-      mb="0.25rem"
-      p="0.5rem 0rem"
-      color={palette.grey[300]}
-    >
+    <FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[300]}>
       {/* left side */}
-      <FlexBetween gap="0.75rem" >
+      <FlexBetween gap="0.75rem">
+        <CodeIcon sx={{ fontSize: "38px" }} />
+        <Typography variant="h4" fontSize="16px">
+          CodeArena
+        </Typography>
+      </FlexBetween>
 
+      {/* rigth side */}
+      <FlexBetween gap="2rem">
+        {/* pseudo hover */}
+        <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
+          <Link
+            to="/"
+            onClick={() => setSelected("dashboard")}
+            style={{
+              color: selected === "dashboard" ? "inherit" : palette.grey[700],
+              textDecoration: "inherit",
+            }}
+          >
+            dashboard
+          </Link>
+        </Box>
+        <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
+          <Link
+            to="/predictions"
+            onClick={() => setSelected("predictions")}
+            style={{
+              color: selected === "predictions" ? "inherit" : palette.grey[700],
+              textDecoration: "inherit",
+            }}
+          >
+            predictions
+          </Link>
+        </Box>
       </FlexBetween>
     </FlexBetween>
   );
