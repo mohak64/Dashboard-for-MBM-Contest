97145f3892507184b8b6a970db17213309b3dad0
Jaihanuman64 <mohak64bansal@gmail.com>
2023-04-12 14:02:45 +0530

    grid in action

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -13,18 +13,32 @@ const gridTemplate = `
  "g h i"
  "g h j"
  "g h j"
-`
+`;
 
 const Dashboard = (props: Props) => {
   const { palette } = useTheme();
   return (
-    <Box width="100%" height="100%" display="grid" gap="1.5rem" 
-    sx={{
-      gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
-      gridTemplateRows: "repeat(10,minmax(60px, 1fr))",
-      gridTemplateAreas: gridTemplate,
-    }}>
-      Dashboard
+    <Box
+      width="100%"
+      height="100%"
+      display="grid"
+      gap="1.5rem"
+      sx={{
+        gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+        gridTemplateRows: "repeat(10,minmax(60px, 1fr))",
+        gridTemplateAreas: gridTemplate,
+      }}
+    >
+      <Box bgcolor="#fff" gridArea="a"></Box>
+      <Box bgcolor="#fff" gridArea="b"></Box>
+      <Box bgcolor="#fff" gridArea="c"></Box>
+      <Box bgcolor="#fff" gridArea="d"></Box>
+      <Box bgcolor="#fff" gridArea="e"></Box>
+      <Box bgcolor="#fff" gridArea="f"></Box>
+      <Box bgcolor="#fff" gridArea="g"></Box>
+      <Box bgcolor="#fff" gridArea="h"></Box>
+      <Box bgcolor="#fff" gridArea="i"></Box>
+      <Box bgcolor="#fff" gridArea="j"></Box>
     </Box>
   );
 };
