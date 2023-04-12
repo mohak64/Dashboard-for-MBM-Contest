3f92ff55723c277a4189ef488022d0deaa5928d3
Jaihanuman64 <mohak64bansal@gmail.com>
2023-04-12 19:17:34 +0530

    restructuring the dashboard folder

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,8 +1,10 @@
-import DashboardBox from "@/components/DashboardBox";
+
 import { Box, useMediaQuery, useTheme } from "@mui/material";
+import Row1 from "./Row1";
+import Row2 from "./Row2";
+import Row3 from "./Row3";
 
 
-type Props = {};
 
 const gridTemplateLargeScreens = `
  "a b c"
@@ -50,7 +52,7 @@ const gridTemplateSmallScreens = `
  "j"
 `;
 
-const Dashboard = (props: Props) => {
+const Dashboard = () => {
   const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
   const { palette } = useTheme();
   return (
@@ -73,16 +75,9 @@ const Dashboard = (props: Props) => {
             }
       }
     >
-      <DashboardBox gridArea="b"></DashboardBox>
-      <DashboardBox gridArea="a"></DashboardBox>
-      <DashboardBox gridArea="c"></DashboardBox>
-      <DashboardBox gridArea="d"></DashboardBox>
-      <DashboardBox gridArea="e"></DashboardBox>
-      <DashboardBox gridArea="f"></DashboardBox>
-      <DashboardBox gridArea="g"></DashboardBox>
-      <DashboardBox gridArea="h"></DashboardBox>
-      <DashboardBox gridArea="i"></DashboardBox>
-      <DashboardBox gridArea="j"></DashboardBox>
+      <Row1 />
+      <Row2 />
+      <Row3 />
     </Box>
   );
 };
