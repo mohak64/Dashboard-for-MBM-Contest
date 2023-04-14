1e196c374d56f5d03f731f7e2269195dce19d9b7
Jaihanuman64 <mohak64bansal@gmail.com>
2023-04-14 20:36:06 +0530

    checking api call

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -6,6 +6,8 @@ type Props = {};
 
 const Row1 = (props: Props) => {
   const {data} = useGetKpisQuery();
+  console.log("🚀 ~ file: Row1.tsx:9 ~ Row1 ~ data:", data)
+  
   return ( 
     <>
       <DashboardBox gridArea="b"></DashboardBox>
