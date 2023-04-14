58606bb3cd5d38a034bacbd4d661b9c833a5bcf0
Jaihanuman64 <mohak64bansal@gmail.com>
2023-04-14 11:35:41 +0530

    backend setup

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import helmet from "helmet";
+import dotenv from "dotenv";
+import morgan from "morgan";
+
+// CONFIGURATION
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+console.log("hello");
+
+// MONGOOSE SETUP
