04e6ba0d149f80e3484642d7f4a3e92465fc1e02
Jaihanuman64 <mohak64bansal@gmail.com>
2023-04-15 20:08:41 +0530

    fetching product data

--- server/index.js
+++ server/index.js
@@ -7,7 +7,9 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js";
 import KPI from "./models/KPI.js";
-import { kpis } from "./data/data.js";
+import productRoutes from "./routes/product.js";
+import Product from "./models/Product.js"
+import { kpis, products } from "./data/data.js";
 
 // CONFIGURATION
 dotenv.config();
@@ -23,6 +25,7 @@ console.log("hello");
 
 // ROUTES
 app.use("/kpi", kpiRoutes);
+app.use("/product", productRoutes);
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 9000;
@@ -37,7 +40,7 @@ mongoose
     /* ADD DATA ONE TIME ONLY OR AS NEEDED */
     // await mongoose.connection.db.dropDatabase();
     // KPI.insertMany(kpis);
-    // Product.insertMany(products);
+    //Product.insertMany(products);
     // Transaction.insertMany(transactions);
   })
   .catch((error) => console.log(`${error} did not connect`));
