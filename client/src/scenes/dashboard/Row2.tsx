d916a1d6f1ee0637a775f78990db8c20315aa71f
Jaihanuman64 <mohak64bansal@gmail.com>
2023-04-16 12:27:27 +0530

    4th chart complete

--- client/src/scenes/dashboard/Row2.tsx
+++ client/src/scenes/dashboard/Row2.tsx
@@ -1,13 +1,101 @@
+import BoxHeader from "@/components/BoxHeader";
 import DashboardBox from "@/components/DashboardBox";
-import React from "react";
+import { useGetKpisQuery, useGetProductsQuery } from "@/state/api";
+import React, { useMemo } from "react";
+
+import { Box, Typography, useTheme } from "@mui/material";
+import {
+  ResponsiveContainer,
+  AreaChart,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Legend,
+  Tooltip,
+  Area,
+  CartesianGrid,
+  BarChart,
+  Bar,
+} from "recharts";
 
 type Props = {};
 
 const Row1 = (props: Props) => {
+  const { palette } = useTheme();
+  const { data: operationalData } = useGetKpisQuery();
+  const { data: productData } = useGetProductsQuery();
+
+  const operationalExpenses = useMemo(() => {
+    return (
+      operationalData &&
+      operationalData[0].monthlyData.map(({ month, operationalExpenses , nonOperationalExpenses }) => {
+        return {
+          name: month.substring(0, 3),
+          "Operational Expenses": operationalExpenses,
+          "Non Operational Expenses": nonOperationalExpenses,
+        };
+      })
+    );
+  }, [operationalData]);
+
   return (
     <>
-      <DashboardBox gridArea="d"></DashboardBox>
-      <DashboardBox gridArea="e"></DashboardBox>
+      <DashboardBox gridArea="d">
+        <BoxHeader
+          title="Correct vs Incorrect Submissions"
+          subtitle=""
+          sideText="+4%"
+        />
+        <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+            data={operationalExpenses}
+            margin={{
+              top: 20,
+              right: 0,
+              left: -10,
+              bottom: 55,
+            }}
+          >
+            <CartesianGrid vertical={false} stroke={palette.grey[800]} />
+            <XAxis
+              dataKey="name"
+              tickLine={false}
+              style={{ fontSize: "10px" }}
+            />
+            <YAxis
+              yAxisId="left"
+              orientation="left"
+              tickLine={false}
+              axisLine={false}
+              style={{ fontSize: "10px" }}
+            />
+            <YAxis
+              yAxisId="right"
+              orientation="right"
+              tickLine={false}
+              axisLine={false}
+              style={{ fontSize: "10px" }}
+            />
+            <Tooltip />
+            <Line
+              yAxisId="left"
+              type="monotone"
+              dataKey="Non Operational Expenses"
+              stroke={palette.tertiary[500]}
+            />
+            <Line
+              yAxisId="right"
+              type="monotone"
+              dataKey="Operational Expenses"
+              stroke={palette.primary.main}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </DashboardBox>
+      <DashboardBox gridArea="e">
+        
+      </DashboardBox>
       <DashboardBox gridArea="f"></DashboardBox>
     </>
   );
